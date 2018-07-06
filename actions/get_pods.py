b35d0e641643c673bad96ea74c0f30ed3529a07a
yang.wang <yang.wang@vungle.com>
2018-07-06 13:41:28 +0800

    get pods

--- actions/get_pods.py
+++ actions/get_pods.py
@@ -1,10 +1,15 @@
 from kubernetes import client, config
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
+from st2common.runners.base_action import Action
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_namespaced_pod(namespace, watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+class get_pods(Action):
+    def run(
+            self,
+            namespace=None,
+            labels=None):
+
+        # Configs can be set in Configuration class directly or using helper utility
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        ret = v1.list_namespaced_pod(namespace, watch=False)
+        return (True, ret.items)
