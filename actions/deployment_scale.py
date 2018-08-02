6e38db5f6b78323e7a21056c87b7dd3b279e261b
yang.wang <yang.wang@vungle.com>
2018-08-02 11:26:57 +0800

    test scale deployment

---
+++ actions/deployment_scale.py
@@ -0,0 +1,24 @@
+from kubernetes import client, config
+
+from st2common.runners.base_action import Action
+
+class deployment_scale(Action):
+    def run(
+            self,
+            namespace=None,
+            number=None,
+            min=None,
+            max=None,
+            deploy_name=None):
+
+        # Configs can be set in Configuration class directly or using helper utility
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        body = read_namespaced_deployment_scale(deploy_name, namespace, pretty=pretty)
+        return (True, body)
+        #if number > min && number < max:
+        #    body.number = number
+        #else:
+        #	return (False, 'bad number')
+        #api_reponse = v1.patch_namespaced_deployment_scale(deploy_name, namespace, body, pretty=pretty)
+        #return (True, api_reponse)
