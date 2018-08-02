ec125a31bbc7a3d84a78cad728bff11c73bbd437
yang.wang <yang.wang@vungle.com>
2018-08-02 13:20:04 +0800

    test

--- actions/deployment_scale.py
+++ actions/deployment_scale.py
@@ -14,11 +14,11 @@ class deployment_scale(Action):
         # Configs can be set in Configuration class directly or using helper utility
         config.load_kube_config()
         v1 = client.AppsV1Api()
-        body = read_namespaced_deployment_scale(deploy_name, namespace, pretty=pretty)
-        return (True, body)
-        #if number > min && number < max:
-        #    body.number = number
-        #else:
-        #	return (False, 'bad number')
+        body = v1.read_namespaced_deployment_scale(deploy_name, namespace)
+        if number >= min and number <= max:
+            body.spec.replicas = number
+        else:
+                return (False, 'bad number')
         #api_reponse = v1.patch_namespaced_deployment_scale(deploy_name, namespace, body, pretty=pretty)
+        return (True, body)
         #return (True, api_reponse)
